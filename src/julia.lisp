7b454669053dc2da000ca9d65053544adb2e7586
Noam Zeise <noam4134@gmail.com>
2023-05-12 18:59:04 +0100

    have mandelbrot fn use julia fn

--- src/julia.lisp
+++ src/julia.lisp
@@ -2,13 +2,14 @@
 ;; julia set functions -WIP
 
 (defun expt-nothrow (x pow)
-  "goes to zero in case of float overflow"
+  "exponent function that returns zero in the case of float overflow"
   (handler-case (expt x pow)
     (floating-point-overflow ()
       (format t "float overflow x:~a~%" x)
       0.0d0)))
 
 (defun compexpt (c pow)
+  "bring the imag and real part of a complex number to a given power and sum"
   (handler-case
       (+ (expt-nothrow (realpart c) pow) (expt-nothrow (imagpart c) pow))
     (floating-point-overflow ()
@@ -16,6 +17,7 @@
       0.0d0)))
 
 (defun julia (z n c R iter-max)
+  "compute how much a certain point is in the julia set."
   (/ (dotimes (i iter-max i)
        (if (>= (compexpt z 2.0) (expt R 2.0))
 	   (return i))
@@ -25,8 +27,38 @@
 	 (setf z (complex temp (+ (* z-size (sin inner-trig)) (imagpart c))))))
      iter-max))
 
-(defun julia-pixel (x y scale)
-  (let* ((n 2) (c (complex 0.285d0 0.01d0)) (R 2.0d0)
-	(z (complex x y))
-	 (p (floor (* 255 (julia z n c R (iter-scale-default scale))))))
+(defun iter-scale-default (scale)
+  "returns an amount of iterations for how detailed the julia set needs to be based on the scale of the image"
+  (round (+ (/ 50 (sqrt (* 0.25 scale))) 80)))
+
+(defun julia-pixel (n c R)
+  "return a function that will give you a julia set pixel color"
+  #'(lambda (x y scale)
+      (let* ((z (complex x y))
+	     (p (floor (* 255 (julia z n c R (iter-scale-default scale))))))
+	(make-colour p p p 255))))
+
+
+(defun mandelbrot-pixel (x y scale &key (iter-fn #'iter-scale-default))
+  "given a position and scaling value, generate a color to indicate whether this position is in the mandelbrot
+set or not."
+  (let ((p (floor (* 255 (julia (complex 0 0) 2 (complex x y) 2
+				     (funcall iter-fn scale))))))
     (make-colour p p p 255)))
+
+;;some mandelbrot values:
+;; 0.001 0.4201 -0.2091
+
+;; 1 -1.5 -0.5
+;; 0.001 -1.405 -0.0005
+
+;; 0.001 -1.449 -0.0005
+
+;; little one  - 0.005 -1.45 -0.0025
+
+;; 1 0.2 -0.5
+;; 0.0001 0.25045 -0.00005
+
+
+;;start 3 -2.2 -1.5
+;;end 0.054 -1.794 -0.027
