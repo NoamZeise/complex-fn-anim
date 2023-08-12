18f36c364598d7e811aa6377c72d1591482ee610
Noam Zeise <noam4134@gmail.com>
2023-08-12 18:55:34 +0100

    function scaling issue changing line thickness resolved

--- src/graphing-fns.lisp
+++ src/graphing-fns.lisp
@@ -2,24 +2,33 @@
 
 ;;; left-fn(x, y) = right-fn(x, y)
 (defun closeness (left-fn right-fn)
-  (lambda (x y scale)
-    (let* ((cutoff (/ scale 400))
-	   (left (funcall left-fn x y))
-	   (right (funcall right-fn x y))
-	   (dist (abs (- left right))))
-      
-      (if (> dist cutoff) 1
-	  (/ dist cutoff)))))
+  (flet ((calc-cutoff (fn x y delta)
+	   (max (abs (- (funcall fn (+ x delta) y)
+			(funcall fn (- x delta) y)))
+		(abs (- (funcall fn x (+ y delta))
+			(funcall fn x (- y delta)))))))
+    (lambda (x y scale)
+      (let* ((left (funcall left-fn x y))
+	     (right (funcall right-fn x y))
+	     (dist (abs (- left right)))
+	     (delta (let ((test-change (/ scale 100)))
+		      (* test-change (max (calc-cutoff left-fn x y test-change)
+					  (calc-cutoff right-fn x y test-change)
+					  test-change))))) ;; in case of all cutoffs being 0
+	(if (> dist delta) 1
+	    (/ dist delta))))))
 
 ;;; fn(x, y) = target
-(defun const-closeness (fn c)
-  (closeness fn (lambda (x y) c)))
+(defun const-closeness (fn c &key (flipped nil))
+  (if flipped
+      (closeness (lambda (x y) c) fn)
+      (closeness fn (lambda (x y) c))))
 
 ;;; fn(x) = y
-(defun fn-closeness (fn)
+(defun fn-closeness (fn &key (flipped nil))
   (closeness
-   (lambda (x y) (funcall fn x))
-   (lambda (x y) y)))
+   (lambda (x y) (funcall fn (if flipped y x)))
+   (lambda (x y) (if flipped x y))))
 
 (defun graph-pixel (fn-list)
   (lambda (x y scale) 
